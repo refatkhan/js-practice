5f5221e4a75cb4840064a45a4a5901ba4f234764
Ismail Khan Refat <refatkhan897@gmail.com>
2025-11-14 16:01:48 +0600

    sorting practice

---
+++ index.js
@@ -0,0 +1,9 @@
+const numbers = [40, 50, 26, 4, 6, 10];
+
+const sortNumbers = numbers.sort((a, b) => a - b);
+console.log(sortNumbers);
+console.log(numbers)
+const tagsFromPosts = [[
+    "javascript", "react", "css"
+],
+["node", ]];
